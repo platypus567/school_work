da41b363fa7799e6176a227c06efddd16bf14308
whitehatns <63357002+platypus567@users.noreply.github.com>
2021-12-08 08:02:44 -0500

    Add files via upload

---
+++ chemistryProject.py
@@ -0,0 +1,18 @@
+print("Welcome to Nikhil's chemical bonding project!")
+learnChoice = input("What would you like to learn about?\n Octet rule, covalent bonding, ionic bonding, metallic bonding, electronegativity, or names of formulas.")
+if learnChoice.lower() == "octet rule":
+    print("Glad you asked! \nAtoms form bonds because it stabilizes their outer shell. In most cases, Atoms want 8 electrons in their outer shell, and that is the octet rule.\n They will try to bond with atoms that can fulfill that need.")
+elif learnChoice.lower() == "covalent bonding":
+    print("")
+elif learnChoice.lower() == "ionic bonding":
+    print("")
+elif learnChoice.lower() =="metallic bonding":
+    print("")
+elif learnChoice.lower() =="electronegativity":
+    print("")
+elif learnChoice.lower() =="names of formulas" or "names":
+    print("")
+
+
+
+
